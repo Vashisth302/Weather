3aded6c663105f0edc32386f9326e6ae3601a661
vashisthji302 <vashisthji302@gmail.com>
2025-11-06 12:17:32 +0530

    commit

--- config.js
+++
@@ -1,3 +0,0 @@
-// Add your OpenWeatherMap API key
-// (Don’t commit this in public repos)
-const OWM_API_KEY = "93194c745e247ffee4086bfcd2228235";
--- config.sample.js
+++
@@ -1,3 +0,0 @@
-// config.sample.js
-// Copy this file to config.js and insert your own API key.
-const OWM_API_KEY = "YOUR_API_KEY_HERE";
